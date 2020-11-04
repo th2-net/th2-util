b2f659c3292d68a4c169a1c1958530a581f01967
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2020-11-04 13:36:10 +0300

    [TH2-931] Migrated to schema API

--- src/main/java/com/exactpro/th2/utility/MicroserviceMain.java
+++ src/main/java/com/exactpro/th2/utility/MicroserviceMain.java
@@ -15,59 +15,89 @@
  */
 package com.exactpro.th2.utility;
 
+import static com.exactpro.th2.common.metrics.CommonMetrics.setLiveness;
+import static com.exactpro.th2.common.metrics.CommonMetrics.setReadiness;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import java.io.IOException;
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.exactpro.th2.configuration.Configuration;
-import com.exactpro.th2.utility.configuration.UtilityConfiguration;
+import com.exactpro.th2.common.schema.factory.CommonFactory;
 
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 
 public class MicroserviceMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(MicroserviceMain.class);
 
-    /**
-     * Environment variables:
-     *  {@link Configuration#ENV_GRPC_PORT}
-     */
     public static void main(String[] args) {
+        Deque<AutoCloseable> resources = new ConcurrentLinkedDeque<>();
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+
+        configureShutdownHook(resources, lock, condition);
         try {
-            Configuration configuration = new UtilityConfiguration();
-
-            Server grpcServer = ServerBuilder.forPort(configuration.getPort())
-                    .addService(new MessageComparatorService())
-                    .build();
-
-            configureShutdownHook(grpcServer);
-
-            grpcServer.start();
-            LOGGER.info("Utility started");
-            grpcServer.awaitTermination();
-        } catch (RuntimeException | InterruptedException | IOException e) {
-            LOGGER.error("Utility crashed", e);
-            System.exit(-1);
-        }
-    }
-
-    private static void configureShutdownHook(Server gRPCServer) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                LOGGER.info("Utility is terminating");
+            setLiveness(true);
+            CommonFactory factory = CommonFactory.createFromArguments(args);
+            resources.add(factory);
+            Server gRPCServer = factory.getGrpcRouter().startServer(new MessageComparatorService());
+            resources.add(() -> {
                 gRPCServer.shutdown();
                 if (!gRPCServer.awaitTermination(3, SECONDS)) {
                     LOGGER.info("Utility isn't terminated gracefully");
                     gRPCServer.shutdownNow();
                 }
-            } catch (InterruptedException e) {
-                LOGGER.error("gRPC server shutdown is interrupted", e);
-            } finally {
-                LOGGER.info("Utility terminated");
+            });
+            gRPCServer.start();
+            setReadiness(true);
+            LOGGER.info("util store started");
+            awaitShutdown(lock, condition);
+        } catch (InterruptedException e) {
+            LOGGER.info("The main thread interupted", e);
+        } catch (Exception e) {
+            LOGGER.error("Fatal error: {}", e.getMessage(), e);
+            System.exit(1);
+        }
+    }
+
+    private static void awaitShutdown(ReentrantLock lock, Condition condition) throws InterruptedException {
+        try {
+            lock.lock();
+            LOGGER.info("Wait shutdown");
+            condition.await();
+            LOGGER.info("App shutdowned");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private static void configureShutdownHook(Deque<AutoCloseable> resources, ReentrantLock lock, Condition condition) {
+        Runtime.getRuntime().addShutdownHook(new Thread("Shutdown hook") {
+            @Override
+            public void run() {
+                LOGGER.info("Shutdown start");
+                setReadiness(false);
+                try {
+                    lock.lock();
+                    condition.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+
+                resources.descendingIterator().forEachRemaining(resource -> {
+                    try {
+                        resource.close();
+                    } catch (Exception e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                });
+                setLiveness(false);
+                LOGGER.info("Shutdown end");
             }
-        }));
+        });
     }
 }
